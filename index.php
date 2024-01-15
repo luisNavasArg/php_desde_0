10f7bcd56c99df83cd408255ee625bca6e782737
luisNavasArg <lpipnavas@gmail.com>
2024-01-15 15:42:54 -0300

    TRabajamos con las cadenas de textos y condionales if y switch  y funciones

--- index.php
+++ index.php
@@ -13,7 +13,10 @@ $dia="Domingo";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Clase 1</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" 
+    rel="stylesheet" 
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" 
+    crossorigin="anonymous">
 </head>
 <body>
     <h1>Clase 1</h1>
@@ -49,8 +52,9 @@ $dia="Domingo";
      }
      ?><h3>Condicional ternario</h3>
      <?php
-     echo $es_estudiante?"<h4 class='bg-success'><br>Hola estudiante</h4>":
-                        "<h4 class='bg-danger'><br>Hola Profesor</h4>";
+     echo $es_estudiante?
+     "<h4 class='bg-success'><br>Hola estudiante</h4>":
+      "<h4 class='bg-danger'><br>Hola Profesor</h4>";
     
                         ?><h3>Bucles</h3>
                         <?php
@@ -75,5 +79,137 @@ $dia="Domingo";
         $contador++;
      } while ($contador <3);
      ?></h2>
+      <h1>Manipulación de cadenas y arrays</h1>
+     <h2>Clase 2 </h2>
+     <?php
+     $saludo="Hola, ";
+     $nombre="Luis Navas";
+     $mensaje=$saludo.$nombre;
+     ?>
+     <h4><?=$mensaje?></h4>
+     <?php $longitud=strlen($mensaje)?>
+     <p>La longitud <?=$longitud?></p>
+
+     <h4>Sub cadenas</h4>
+     <?php
+     $frase = "Bienvenidos a nuestra web";
+     $buscar="sol";
+     $pos=strpos($frase,$buscar);
+     
+     if ($pos) {
+      echo "<p>Nuestra frase buscada comienza en la posición :".$pos."</p>";
+     }else{
+      echo "<br><p>La frase buscada no se encontró";
+     }
+     if (22 < strlen($frase)) {
+      ?>
+      <p><?= $frase[22]?></p>
+      <?php
+     }
+     $fecha="2024-01-25";
+
+     ?>
+     <p>La longitud de una fechas : <?=strlen($fecha)?></p>
+     <?php
+     if(strlen($fecha)===10){
+      echo "Es valida esta fecha ".$fecha;
+     }
+     $arra1 = str_split($frase);
+     $arra2 = str_split($frase,13);
+     echo "<br>";
+     print_r($arra1);
+     echo "<br>";
+     print_r($arra2);
+     echo "<br>";
+     //Buscar en una cadena si existe la frase y si es identica
+     function buscar_frase($frase_a_buscar,$frase){
+      // return  $frase_a_buscar." ".$frase;
+      // return strlen($frase_a_buscar)." ".strlen($frase);
+      if (strlen($frase_a_buscar) < strlen($frase)) {
+         if (strpos($frase,$frase_a_buscar)) {
+            return $frase_a_buscar;
+         }else{
+            return "La frase no se encuentra";
+         }
+      }else{
+         return "la frase a buscar es menor que la cadena de texto";
+      }
+
+     }
+     $palabra=buscar_frase("día","Hola buen día");
+     echo $palabra;echo "<br>";
+     function dia_laborables($dia,$no_es_feriado,$hora){
+      // if(($dia=="Lunes") || ($dia=="Martes")||($dia=="Miércoles")||
+      // ($dia=="Jueves")||($dia=="Viernes")){
+      //    if ($no_es_feriado) {
+      //       return true;
+      //    }else{
+      //       return false;
+      //    }         
+      // }else{
+         
+         
+      //    return (false);
+      // }
+     switch ($dia) {
+      case 'Lunes':
+         if ($no_es_feriado) {
+            if (($hora<"15:00")&&($hora>="10:00")) {
+               return array("msg"=>"Es laborable esta abierto");
+            }else{
+               return array("msg"=>"Es laborable pero esta fuera de horario");
+            }
+                  
+               }else{
+                  return array("msg"=>"Es feriado");
+               } 
+         break;
+         case 'Martes':
+            if ($no_es_feriado) {
+                     return true;
+                  }else{
+                     return false;
+                  } 
+            break;
+            case 'Miércoles':
+               if ($no_es_feriado) {
+                        return true;
+                     }else{
+                        return false;
+                     } 
+               break;
+               case 'Jueves':
+                  if ($no_es_feriado) {
+                           return true;
+                        }else{
+                           return false;
+                        } 
+                  break;
+                  case 'Viernes':
+                     if ($no_es_feriado) {
+                              return true;
+                           }else{
+                              return false;
+                           } 
+                     break;
+      default:
+         return false;
+         break;
+     }
+     }
+     $respuesta= dia_laborables("Lunes",true,"19:00");
+     echo $respuesta["msg"];
+     
+
+     // El resultado de la expresión (true && false) es asignado a $g
+// Actúa como: ($g = (true && false))
+$g = true && false;
+
+// La constante true es asignada a $h antes de que suceda la operación "and"
+// Actúa como: (($h = true) and false)
+$h = true and false;
+
+// var_dump($h);
+     ?> 
 </body>
 </html>
